2f98bb828092dd31dfa1f2a046c50b2d9701f35f
ellie <ellieyhc@gmail.com>
2019-02-18 21:21:57 -0800

    Firebase base.js file

---
+++ src/base.js
@@ -0,0 +1,16 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDqVXvVWcbWqNGo5Zg16lB1iPzlr0dFEoQ",
+    authDomain: "research-ucla.firebaseapp.com",
+    databaseURL: "https://research-ucla.firebaseio.com",
+})
+
+const base = Rebase.createClass(firebaseApp.database());
+
+// This is a named export
+export {firebaseApp};
+
+// This is a default export
+export default base;
