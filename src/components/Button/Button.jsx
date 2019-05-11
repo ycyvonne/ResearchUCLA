882f2fc7913a78963b1ef7e86ef22c8182051678
ellie <ellieyhc@gmail.com>
2019-05-10 18:53:05 -0700

    Added edit info popup content

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import './style.scss';
 
 class Button extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="button-div"
                 style={{backgroundColor: this.props.backgroundColor,
                         color: this.props.textColor,
-                        borderColor: this.props.borderColor }}
+                        borderColor: this.props.borderColor,
+                        "&:hover": {
+                            backgroundColor: "red",
+                            color: this.props.hoverTextColor
+                        }}}
                 onClick={this.props.onClick}>
                 {this.props.children}
             </div>
