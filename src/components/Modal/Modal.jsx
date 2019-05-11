20c1cfd078afb774a89151b01fa8cfe79bf30941
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-05-10 18:45:46 -0700

    Completely Working Modal!
    Completely Working Version! Two main types:  in-place and entire-screen

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './style.scss';
 import "../../styles/vars.scss";
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Modal extends Component {
 
@@ -16,19 +18,22 @@ class Modal extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({open: nextProps.open})
+        this.setState({
+            open: nextProps.open,
+            type: nextProps.type,  
+            background_color: nextProps.background_color,
+            blur: nextProps.blur 
+        })
     }
 
     show = () => {
-        this.setState({ open: true },() => {
-            document.addEventListener('click', this.close);
-          });
+        document.addEventListener('click', this.handleClick);
         document.body.classList.add('modal-visible');
     }
 
     close = () => {
         this.setState({ open: false },() => {
-            document.removeEventListener('click', this.closeMenu);
+            document.removeEventListener('click', this.handleClick);
           });
         document.body.classList.remove('modal-visible');
         this.props.closeModal();
@@ -39,20 +44,43 @@ class Modal extends Component {
     // if (e.target.className === "modal-wrapper") {
     //     this.close();
     // }
-        if (!this.node.contains(e.target)){
-            this.close();
+        if (this.node.contains(e.target)){
+            return;
         }
+
+        this.close();
     }
 
     render() {
-        return (
+
+        {if(this.state.open)
+            this.show()}
+        const mdl=this.state.type=='entire-screen'?(
             <div>
-                <div style={{display: this.state.open ? 'inline' : 'none'}} onClick={this.handleClick}>
-                    <div style={{opacity: this.state.blur ? 0.80 : 0}} className="modal-filter" />
+                <div style={{display: this.state.open ? 'inline' : 'none'}} >
+                    <div style={{display: this.state.blur ? 'inline' : 'none'}} className="modal-filter" />
                     <div className="modal-wrapper">
-                        <div ref={node=>{this.node=node}} style={{background: this.state.background_color, margin: this.state.type=='entire-screen'? 120 : 0}}className="modal" >
+                        <div ref={node=>{this.node=node}} style={{background: this.state.background_color}}className="modal" >
                             <div className="button-wrapper">
-                                <p className="close-button" onClick={this.close} >x</p>
+                            <FontAwesomeIcon
+                            className= "close-button"
+                            icon={faTimesCircle}  onClick={this.close}/>
+                            </div>
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ):
+        (
+            <div>
+                <div  style={{display: this.state.open ? 'inline' : 'none'}} >
+                    <div ref={node=>{this.node=node}} className="inline-modal-wrapper">
+                        <div  style={{background: this.state.background_color}}className="modal" >
+                            <div className="button-wrapper">
+                            <FontAwesomeIcon
+                            className= "close-button"
+                            icon={faTimesCircle}  onClick={this.close}/>
                             </div>
                             {this.props.children}
                         </div>
@@ -60,6 +88,9 @@ class Modal extends Component {
                 </div>
             </div>
         );
+            
+        
+        return mdl;
     }
 }
 
