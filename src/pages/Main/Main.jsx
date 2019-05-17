558042d8bedf25a7cb19d85ee9d6d909786da5c2
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-05-17 00:24:27 -0700

    Update Main.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,7 +4,6 @@ import FacultyPage from '../../pages/FacultyPage/FacultyPage';
 import ResearchPage from '../../pages/ResearchPage/ResearchPage';
 import NewsPage from '../../pages/NewsPage/NewsPage';
 import ResourcesPage from '../../pages/ResourcesPage/ResourcesPage';
-import LoginPage from '../../pages/LoginPage/LoginPage';
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import Page from '../../components/Page/Page';
 import base from '../../base';
